bdbb222c99dcf497b910213e91d3516aebb08d41
Kenneth Jones <github@kennethdjones.com>
2022-08-16 16:42:39 -0500

    Update import.js

--- import.js
+++ import.js
@@ -1,7 +1,6 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-// lot 104 geometry
 var url = "https://services6.arcgis.com/i4Ofa92MsoSleULR/ArcGIS/rest/services/IUPUI_PermitParking/FeatureServer/2/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&returnCentroid=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=";
 
 
